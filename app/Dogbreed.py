c44da330cdf69b944afb984db1db243d559bc2ec
welldone <welldoneit90@gmail.com>
2022-07-30 19:46:25 +0200

    reupdate

---
+++ app/Dogbreed.py
@@ -0,0 +1,10 @@
+import logging
+import tensorflow as tf
+
+
+class Dogbreed(object):
+    def __init__(self, models_dir='/models'):
+        pass
+
+    def predict(self, x, feature_names=None):
+        pass
